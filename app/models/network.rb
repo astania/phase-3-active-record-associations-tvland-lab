8064bb564bc9b0b3befbb3607c9a256c3d69e7b9
+ <+>
2022-05-18 13:31:31 -0400

    Done.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,11 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+  def network 
+    self.network.call_letters
+  end 
+
+  def sorry 
+    "We're sorry about passing on John Mulaney's pilot"
+  end 
 end
